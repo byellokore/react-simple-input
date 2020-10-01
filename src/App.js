74aaf57c6e41b48c843252f23b4b92c7b922990c
gabriel fontoura dos santos <gabrielfontoura@me.com>
2020-10-01 19:04:24 -0300

    feat: working example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+
+  const charlist = userInput.split('').map((char, index) => {
+      return <Char
+                character={char}
+                key={index}
+                clicked={() => deleteCharHandler(index)}/>;
+  });
+
+  const deleteCharHandler = (index) => {
+    const text = userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    setUserInput(updatedText);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text"
+             onChange={(event) => setUserInput(event.target.value)}
+             value={userInput}    />
+      <p>{userInput}</p>
+      <Validation inputLength={userInput.length} />
+      {charlist}
     </div>
   );
 }
